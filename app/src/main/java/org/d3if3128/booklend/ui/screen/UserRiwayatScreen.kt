84f9b3dac2bc02f7a8f51c1017738984244c1f3d
Dyna Rosalina Pangaribuan <pangaribuandyna@gmail.com>
2024-06-09 21:49:51 +0700

    Perbaikan tampilan UserRiwayatScreen

--- app/src/main/java/org/d3if3128/booklend/ui/screen/UserRiwayatScreen.kt
+++ app/src/main/java/org/d3if3128/booklend/ui/screen/UserRiwayatScreen.kt
@@ -24,6 +24,8 @@ import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.Person
 import androidx.compose.material3.Badge
 import androidx.compose.material3.BadgedBox
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
@@ -47,6 +49,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -58,8 +61,6 @@ import androidx.navigation.compose.rememberNavController
 import coil.compose.rememberAsyncImagePainter
 import org.d3if3128.booklend.R
 import org.d3if3128.booklend.database.BooklendDb
-import org.d3if3128.booklend.model.Buku
-import org.d3if3128.booklend.model.Peminjaman
 import org.d3if3128.booklend.model.User
 import org.d3if3128.booklend.navigation.Screen
 import org.d3if3128.booklend.network.UserDataStore
@@ -68,7 +69,7 @@ import org.d3if3128.booklend.util.ViewModelFactoryPeminjaman
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun UserRiwayatScreen(navController: NavHostController, user: User? = null){
+fun UserRiwayatScreen(navController: NavHostController){
 
     val context = LocalContext.current
     val dataStore = UserDataStore(context)
@@ -175,7 +176,6 @@ fun UserRiwayatScreen(navController: NavHostController, user: User? = null){
         UserRiwayatScreenContent(
             modifier = Modifier.padding(padding),
             userEmail = userData.email,
-            navController = navController
         )
 
     }
@@ -186,7 +186,6 @@ fun UserRiwayatScreen(navController: NavHostController, user: User? = null){
 fun UserRiwayatScreenContent(
     modifier: Modifier,
     userEmail: String,
-    navController: NavHostController
 ) {
     val context = LocalContext.current
     val db = BooklendDb.getInstance(context)
@@ -222,50 +221,93 @@ fun UserRiwayatScreenContent(
 fun ListPeminjaman(peminjamanWithDetails: PeminjamanWithDetails) {
     val peminjaman = peminjamanWithDetails.peminjaman
     val buku = peminjamanWithDetails.buku
-    val user = peminjamanWithDetails.user
 
-    Row(
+    val statusTextColor = if (peminjaman.status == "Ditolak") Color.Red else Color(0xFF2587DC)
+
+    Card(
         modifier = Modifier
-            .padding(16.dp)
+            .padding(bottom = 16.dp)
             .fillMaxWidth(),
-        horizontalArrangement = Arrangement.spacedBy(16.dp, Alignment.Start),
-        verticalAlignment = Alignment.CenterVertically
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 6.dp,
+        ),
+        colors = CardDefaults.cardColors(
+            containerColor = Color.White,
+        ),
+        shape = RoundedCornerShape(8.dp)
     ) {
-        val imageUri = Uri.parse(buku.gambarbuku)
-        Image(
-            painter = rememberAsyncImagePainter(model = imageUri),
-            contentDescription = null,
-            modifier = Modifier
-                .width(71.dp)
-                .height(108.dp)
-                .clip(RoundedCornerShape(8.dp)),
-            contentScale = ContentScale.FillBounds
-        )
         Column(
             verticalArrangement = Arrangement.spacedBy(5.dp, Alignment.Top),
-            horizontalAlignment = Alignment.Start
+            horizontalAlignment = Alignment.Start,
         ) {
-            Text(text = user.iduser.toString())
-            Text(text = user.email)
-            Text(
-                text = buku.judulbuku,
-                maxLines = 2,
-                overflow = TextOverflow.Ellipsis,
-                fontWeight = FontWeight.Bold
-            )
-            Text(
-                text = buku.penulisbuku,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis
-            )
-            Text(
-                text = buku.genrebuku,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis
-            )
-            Text(text = peminjaman.status)
-            Text(text = peminjaman.idpeminjaman.toString())
-            Text(text = peminjaman.tanggalpinjam)
+            Row(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(16.dp, Alignment.Start),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Text(
+                    text = peminjaman.status,
+                    style = TextStyle(
+                        fontSize = 14.sp,
+                        lineHeight = 22.sp,
+                        fontWeight = FontWeight(500),
+                        color = statusTextColor,
+                    )
+                )
+                Text(
+                    text = "Kode Pinjam : " + peminjaman.idpeminjaman.toString(),
+                    style = TextStyle(
+                        fontSize = 14.sp,
+                        lineHeight = 22.sp,
+                        fontWeight = FontWeight(400),
+                        color = Color(0xFF9D9EA8),
+                        textAlign = TextAlign.Right,
+                    )
+                )
+            }
+
+            Row(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(16.dp, Alignment.Start),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                val imageUri = Uri.parse(buku.gambarbuku)
+                Image(
+                    painter = rememberAsyncImagePainter(model = imageUri),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .width(71.dp)
+                        .height(108.dp)
+                        .clip(RoundedCornerShape(8.dp)),
+                    contentScale = ContentScale.FillBounds
+                )
+                Column(
+                    verticalArrangement = Arrangement.spacedBy(5.dp, Alignment.Top),
+                    horizontalAlignment = Alignment.Start
+                ) {
+                    Text(
+                        text = "Judul Buku : " + buku.judulbuku,
+                        maxLines = 2,
+                        overflow = TextOverflow.Ellipsis,
+                        fontWeight = FontWeight.Bold
+                    )
+                    Text(
+                        text = "Penulis : " + buku.penulisbuku,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                    Text(
+                        text = "Genre : " + buku.genrebuku,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                    Text(text = "Tanggal Peminjaman : " + peminjaman.tanggalpinjam)
+                }
+            }
         }
     }
 }
